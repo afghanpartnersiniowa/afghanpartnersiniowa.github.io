55e728a4a39cb261693ff1ba954a06ec03da60db
joeyhage <3220319+joeyhage@users.noreply.github.com>
2022-12-10 23:58:54 -0600

    new recaptcha site key

--- src/_data/env.js
+++ src/_data/env.js
@@ -1,12 +1,13 @@
 const environment = process.env.ELEVENTY_ENV;
-const DEV_ENV = 'dev';
-const devUrl = 'http://localhost:8080';
-const prodUrl = 'https://www.afghanpartnersiniowa.org';
+const DEV_ENV = "dev";
+const devUrl = "http://localhost:8080";
+const prodUrl = "https://www.afghanpartnersiniowa.org";
 const isProd = environment !== DEV_ENV;
 const url = isProd ? prodUrl : devUrl;
 
 module.exports = {
-    environment,
-    isProd,
-    url,
+  environment,
+  isProd,
+  url,
+  recaptchaSiteKey: "6LeZxm4jAAAAALiSYjBVXZ1lDcCJv8OfuIfURIKH",
 };
