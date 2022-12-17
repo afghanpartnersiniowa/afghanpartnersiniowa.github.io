72c3416d118f8a58a584c0c3895eb9af6278f7a1
joeyhage <3220319+joeyhage@users.noreply.github.com>
2022-12-16 18:04:01 -0600

    building home page

--- src/_data/env.js
+++ src/_data/env.js
@@ -1,13 +1,30 @@
 const environment = process.env.ELEVENTY_ENV;
+const nodeEnv = process.env.NODE_ENV;
 const DEV_ENV = "dev";
+const PREVIEW_ENV = "preview";
 const devUrl = "http://localhost:8080";
+const previewUrl = "https://preview.afghanpartnersiniowa.org";
 const prodUrl = "https://www.afghanpartnersiniowa.org";
-const isProd = environment !== DEV_ENV;
-const url = isProd ? prodUrl : devUrl;
+
+let isProd = false;
+let isPreview = false;
+let url;
+
+if (environment === DEV_ENV) {
+  url = devUrl;
+} else if (nodeEnv === PREVIEW_ENV) {
+  url = previewUrl;
+  isProd = true;
+  isPreview = true;
+} else {
+  url = prodUrl;
+  isProd = true;
+}
 
 module.exports = {
   environment,
   isProd,
+  isPreview,
   url,
   recaptchaSiteKey: "6LeZxm4jAAAAALiSYjBVXZ1lDcCJv8OfuIfURIKH",
 };
